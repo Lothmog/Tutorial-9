222cf0be5bca5a00eca022c15ad78e0a7598d242
SMWeakley1s <SMWeakley1s@capetigers.com>
2024-10-04 09:58:12 -0500

    second commit

--- script/tny_script.js
+++ script/tny_script.js
@@ -19,25 +19,21 @@ setInterval("runClock()",1000);
 /*function to create and run the countdown clock*/
 function runClock(){
 
-/*Store the current date and time*/
+
 var currentDay=new Date()
 var dateStr = currentDay.toLocaleDateString();
 var timeStr = currentDay.toLocaleTimeString();
 
-/*Display the current date and time*/
 document.getElementById("dateNow").innerHTML =
 dateStr+"<br />"+timeStr;
 
-/*Calculate the days left until January 1st*/
 var newYear = new Date("January 1, 2018");
 var nextYear = currentDay.getFullYear()+1;
 newYear.setFullYear(nextYear);
 var daysLeft = (newYear-currentDay)/(1000*60*60*24);
 
-/*Calculate the hours left in the current day*/
 var hrsLeft=(daysLeft-Math.floor(daysLeft))*24;
 
-/*Calculate the minutes and seconds left in the current day*/
 var minsLeft=(hrsLeft-Math.floor(hrsLeft))*60;
 var secsLeft=(minsLeft-Math.floor(minsLeft))*60;
 
